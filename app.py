e89eac3227c3b3117e923ccaaa9bbe95057aed90
Riley Wheadon <rileywheadon@gmail.com>
2025-09-06 13:57:36 -0700

    Prepare for deployment on Compute Canada resources.

--- app.py
+++ app.py
@@ -23,13 +23,13 @@ import dotenv
 app = Flask(__name__)
 
 # Connect to Valkey/Plumber (Docker)
-# dotenv.load_dotenv()
-# valkey = redis.Redis.from_url(os.getenv("VALKEY_URL"))
-# PLUMBER_URL = "http://plumber:8000"
+dotenv.load_dotenv()
+valkey = redis.Redis.from_url(os.getenv("VALKEY_URL"))
+PLUMBER_URL = "http://plumber:8000"
 
 # Connect to Valkey/Plumber (Local)
-valkey = redis.Redis("localhost", port = 6379)
-PLUMBER_URL = "http://localhost:8000"
+# valkey = redis.Redis("localhost", port = 6379)
+# PLUMBER_URL = "http://localhost:8000"
 
 
 #############
